a3bddbe13d9a13edd81aee2e24b4b0dca2b8f02a
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2026-02-23 21:36:12 +0530

    feat: add read file feature

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("This is the application frameworks lab 04");
+
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function (err, data) {
+ if (err) throw err;
+ console.log(data);
+});
