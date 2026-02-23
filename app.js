480c595ce424ad726013051046dbe7277472faa1
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2026-02-23 21:48:22 +0530

    feat: implement async await function

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const myPromise = new Promise((resolve, reject) => {
- if (condition) {
- resolve('Success!');
- } else {
- reject('Failure!');
- }
-});
-myPromise.then((result) => {
+async function myFunction() {
+ try {
+ const result = await myPromise;
  console.log(result);
-}).catch((error) => {
+ } catch (error) {
  console.log(error);
-});
+ }
+}
+myFunction();
