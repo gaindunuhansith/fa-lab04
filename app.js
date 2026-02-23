b1838b87933aeddcbb7f18ebd21dd45f9ac54aac
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2026-02-23 21:41:46 +0530

    feat: create web server

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-console.log("This is the application frameworks lab 04");
-
-const fs = require('fs');
-fs.readFile('file.txt', 'utf8', function (err, data) {
- if (err) throw err;
- console.log(data);
-});
-
-const fs = require('fs');
-fs.writeFile('file.txt', 'Hello World!', function (err) {
- if (err) throw err;
- console.log('File saved!');
-});
+const http = require('http');
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write('Hello World!');
+ res.end();
+}).listen(8080);
